4b2784db5606d28309c304d3b983b8705113f17d
chuckie b <charliefbowers@gmail.com>
2022-06-26 20:49:47 -0400

    challenge 1

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import { Link, Box } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
 type TNavBar = {
   links: {
     text: string;
@@ -9,6 +9,8 @@ type TNavBar = {
 };
 
 function NavBar({ links }: TNavBar) {
+  const { pathname } = useLocation();
+
   return (
     <Box
       component="aside"
@@ -29,24 +31,30 @@ function NavBar({ links }: TNavBar) {
         <img src="/surelogo.svg" alt="logo"></img>
       </Link>
 
-      {links.map(({ text, href, 'data-testid': dataTestId }) => (
-        <Link
-          component={RouterLink}
-          key={href}
-          to={href}
-          color="#fff"
-          underline="hover"
-          sx={{
-            cursor: 'pointer',
-            '&:not(:last-of-type)': {
-              marginBottom: '16px',
-            },
-          }}
-          data-testid={dataTestId}
-        >
-          {text}
-        </Link>
-      ))}
+      {links.map(({ text, href, 'data-testid': dataTestId }) => {
+        const isActiveLink = pathname === href;
+
+        return (
+          <Link
+            component={RouterLink}
+            key={href}
+            to={href}
+            color="#fff"
+            underline="hover"
+            aria-current={isActiveLink ? 'page' : undefined}
+            sx={{
+              cursor: 'pointer',
+              textDecoration: isActiveLink ? 'underline' : 'none',
+              '&:not(:last-of-type)': {
+                marginBottom: '16px',
+              },
+            }}
+            data-testid={dataTestId}
+          >
+            {isActiveLink ? `> ${text}` : text}
+          </Link>
+        );
+      })}
     </Box>
   );
 }
